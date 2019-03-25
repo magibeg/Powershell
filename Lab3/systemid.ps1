59549d9409b60bdc03d004bc60800ec36697d4c7
magibeg <ian.dublack@gmail.com>
2019-03-25 14:55:08 -0400

    Update systemid.ps1

--- Lab3/systemid.ps1
+++ Lab3/systemid.ps1
@@ -2,14 +2,15 @@
 #
 
 $hostname = hostname
-$ipinfo = Get-NetIPAddress -AddressFamily IPV4|
-                      select ipaddress, @{n="hostname";e={try {[System.Net.Dns]::GetHostEntry($_.ipaddress).hostname} catch {}}}
+$ipinfo = Get-NetIPAddress -AddressFamily IPV4| select ipaddress, @{n="hostname";e={try {[System.Net.Dns]::GetHostEntry($_.ipaddress).hostname} catch {}}}
+$ipinfo2 = Get-NetIPAddress -AddressFamily IPV4| where-object ipaddress -ne 127.0.0.1
 $extipaddress = (curl icanhazip.com).content
 $extname = try {[System.Net.Dns]::GetHostEntry($extipaddress).hostname} catch {}
-$realHostName = nslookup $ipinfo
+$realHostName = nslookup $extipaddress | select-string name
 "
 Hostname:          $hostname
 $ipinfo
+$ipinfo2
 External IP:       $extipaddress
 External Hostname: $extname
 $realHostname
