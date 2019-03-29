94937d718370823e88a5abc376da92267a6cfd17
magibeg <ian.dublack@gmail.com>
2019-03-29 16:46:27 -0400

    WMIObjects added

---
+++ Lab4/WMIObjects.ps1
@@ -0,0 +1,92 @@
+#This program will display various pieces of system information
+
+"###########################################################"
+"##########   Computer System Summary Report    ############"
+"###########################################################"
+"###########################################################"
+""
+"##### Operating System Summary #####"
+$OSInfo = get-wmiobject -class win32_operatingsystem
+""
+"OS Name: " + $OSInfo.Caption
+"OS Version: " + $OSInfo.Version
+""
+"###########################################################"
+""
+"##### System Summary #####"
+""
+$SysInfo = get-wmiobject -class win32_computersystem
+"System Name: " + $SysInfo.Name
+"System Vendor: " + $SysInfo.Manufacturer
+"System Model: " + $SysInfo.Model
+""
+"###########################################################"
+""
+"##### Bios Summary #####"
+""
+$BiosInfo = get-wmiobject -class win32_bios
+"Bios Vendor: " + $BiosInfo.Manufacturer
+"Bios Description: " + $BiosInfo.Description
+"Bios Version: " + $BiosInfo.Version
+""
+"###########################################################"
+""
+"##### CPU Summary #####"
+""
+$HardwareInfo = get-wmiobject -class win32_processor
+"CPU Description: " + $HardwareInfo.Name
+"CPU Max Speed(GHz): " + $HardwareInfo.MaxClockSpeed
+"CPU Core Count: " + $HardwareInfo.NumberOfCores
+"CPU Thread Count: " + $HardwareInfo.ThreadCount
+""
+"###########################################################"
+""
+"##### Memory Summary #####"
+""
+$MemInfo = get-wmiobject -class win32_physicalmemory |
+ForEach-Object {
+    new-object -TypeName psobject -Property @{
+        Manufacturer = $_.manufacturer;
+        Size = $_.capacity/1MB;
+        Bank = $_.banklabel;
+        Slot = $_.DeviceLocator;
+    };
+}
+$MemInfo
+""
+"###########################################################"
+""
+"##### Disk Summary #####"
+get-wmiobject -class win32_diskdrive |
+ForEach-Object {
+    new-object -TypeName psobject -Property @{
+        Manufacturer = $_.Description
+        Model = $_.Model
+        Size = $_.Size/1GB
+    }
+} | Format-Table -AutoSize Manufacturer, Model, Size
+
+
+get-wmiobject -class win32_logicaldisk |
+ForEach-Object {
+    new-object -TypeName psobject -Property @{
+        Drive = $_.Caption
+        "File System Type" = $_.FileSystem
+        "Size (GB)" = $_.Size/1GB
+        "Free Space (GB)" = $_.FreeSpace/1GB
+    }
+} | Format-Table -AutoSize Drive, "File System Type", "Size (GB)", "Free Space (GB)"
+
+"###########################################################"
+""
+"##### Video Card Summary #####"
+""
+$VideoInfo = get-wmiobject -class win32_videocontroller
+"Vendor Name: " + $VideoInfo.AdapterCompatibility
+"GPU Description: " + $VideoInfo.Caption
+"Display Mode: " + $VideoInfo.VideoModeDescription
+""
+"###########################################################"
+"######################  END   #############################"
+"###########################################################"
+""
